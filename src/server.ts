51597054b6e54f04736e809a13381e81a4313f13
Mikael Josias <mikaeljosias2002@gmail.com>
2021-01-15 10:49:05 -0300

    feat(files): create route that show image files

--- src/server.ts
+++ src/server.ts
@@ -2,10 +2,13 @@ import "reflect-metadata";
 import express from 'express';
 import routes from './routes/index';
 import "./database/index";
+import uploadConfig from './config/upload';
+import upload from "./config/upload";
 
 const app = express();
 
 app.use(express.json());
+app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
 
 app.listen(3333, () => {
